cceb37d008a246e9248a6dbb566e0bab1c809c45
Vikas Jayaraman <jaivikas7@gmail.com>
2019-04-02 17:59:44 +0530

    Added CustomerDao File

---
+++ src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
@@ -0,0 +1,89 @@
+package com.tadigital.ecommerce.customer.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.tadigital.ecommerce.customer.entity.Customer;
+
+public class CustomerDao extends Dao {
+	public boolean selectCustomerByEmailAndPassword(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		ResultSet rs = null;
+		
+		try {
+			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "' AND cust_pwd = '" + customer.getPassword() + "'";
+			
+			rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+				status = true;
+				
+				customer.setFirstName(rs.getString(1));
+				customer.setLastName(rs.getString(2));
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeResultSet(rs);
+			closeConnection(con);
+		}
+		
+		return status;
+	}
+	
+	public boolean selectCustomerByEmail(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		ResultSet rs = null;
+		
+		try {
+			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "'";
+			
+			rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+			
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeResultSet(rs);
+			closeConnection(con);
+		}
+		
+		return status;
+	}
+	
+	public boolean insertCustomer(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		try {
+			String sql = "INSERT INTO customer_info(cust_fname, cust_lname, cust_email, cust_pwd) " +
+						 "VALUES('" + customer.getFirstName() + "', '" + customer.getLastName() +
+						 "', '" + customer.getEmail() + "', '" + customer.getPassword() + "')";
+			
+			int rows = stmt.executeUpdate(sql);
+			if(rows != 0) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		return status;
+	}
+}
