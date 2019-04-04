fcb0c2fc4a5b89dd1ff1a35c0998a5162c907cbe
Vikas Jayaraman <jaivikas7@gmail.com>
2019-04-04 18:32:29 +0530

    Added Customer Dao file

--- src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
+++ src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
@@ -10,71 +10,193 @@ import com.tadigital.ecommerce.customer.entity.Customer;
 public class CustomerDao extends Dao {
 	public boolean selectCustomerByEmailAndPassword(Customer customer) {
 		boolean status = false;
-		
+
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
 		ResultSet rs = null;
-		
+
 		try {
-			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "' AND cust_pwd = '" + customer.getPassword() + "'";
-			
+			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "' AND cust_pwd = '"
+					+ customer.getPassword() + "'";
+
 			rs = stmt.executeQuery(sql);
-			if(rs.next()) {
+			if (rs.next()) {
 				status = true;
-				
+
 				customer.setFirstName(rs.getString(1));
 				customer.setLastName(rs.getString(2));
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		} finally {
-			closeStatement(stmt);
 			closeResultSet(rs);
+			closeStatement(stmt);
 			closeConnection(con);
 		}
-		
+
 		return status;
 	}
-	
+
 	public boolean selectCustomerByEmail(Customer customer) {
 		boolean status = false;
-		
+
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
 		ResultSet rs = null;
-		
+
 		try {
 			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "'";
-			
+
 			rs = stmt.executeQuery(sql);
-			if(rs.next()) {
+			if (rs.next()) {
 				status = true;
 			}
 		} catch (SQLException sqle) {
-			
+
 			sqle.printStackTrace();
 		} finally {
-			closeStatement(stmt);
 			closeResultSet(rs);
+			closeStatement(stmt);
 			closeConnection(con);
 		}
-		
+
 		return status;
 	}
-	
-	public boolean insertCustomer(Customer customer) {
+
+	public boolean updatePassword(Customer customer) {
 		boolean status = false;
-		
+
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
-		
+
 		try {
-			String sql = "INSERT INTO customer_info(cust_fname, cust_lname, cust_email, cust_pwd) " +
-						 "VALUES('" + customer.getFirstName() + "', '" + customer.getLastName() +
-						 "', '" + customer.getEmail() + "', '" + customer.getPassword() + "')";
-			
+			String sql = "UPDATE customer_info SET cust_pwd ='" + customer.getPassword() + "' WHERE cust_email = '"
+					+ customer.getEmail() + "'";
+
+			int row = stmt.executeUpdate(sql);
+			if (row != 0) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+
+		return status;
+	}
+
+	public boolean updateCustomerDet(Customer customer, String x) {
+		boolean status = false;
+
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+
+		try {
+
+			StringBuffer sql = new StringBuffer("UPDATE customer_info SET ");
+			boolean flag = false;
+
+			if (customer.getFirstName() != "") {
+				sql.append("cust_fname='" + customer.getFirstName() + "'");
+				flag = true;
+			}
+
+			if (customer.getLastName() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_lname='" + customer.getLastName() + "'");
+				flag = true;
+			}
+
+			if (customer.getGender() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_gen='" + customer.getGender() + "'");
+				flag = true;
+			}
+
+			if (customer.getAddress() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_addr='" + customer.getAddress() + "'");
+				flag = true;
+			}
+
+			if (customer.getCity() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_city='" + customer.getCity() + "'");
+				flag = true;
+			}
+
+			if (customer.getZip() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_zip='" + customer.getZip() + "'");
+				flag = true;
+			}
+
+			if (customer.getState() != null && customer.getState() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_state='" + customer.getState() + "'");
+				flag = true;
+			}
+
+			if (customer.getCountry() != null && customer.getCountry() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_cntry='" + customer.getCountry() + "'");
+				flag = true;
+			}
+
+			if (customer.getContact() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_cont='" + customer.getContact() + "'");
+				flag = true;
+			}
+
+			if (customer.getEmail() != "") {
+				if (flag)
+					sql.append(",");
+				sql.append("cust_email='" + customer.getEmail() + "'");
+				flag = true;
+			}
+
+			sql.append(" WHERE cust_email='" + x + "'");
+
+			int row = stmt.executeUpdate(sql.toString());
+			if (row != 0) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+
+		return status;
+	}
+
+	public boolean insertCustomer(Customer customer) {
+		boolean status = false;
+
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+
+		try {
+			String sql = "INSERT INTO customer_info(cust_fname, cust_lname, cust_email, cust_pwd) " + "VALUES('"
+					+ customer.getFirstName() + "', '" + customer.getLastName() + "', '" + customer.getEmail() + "', '"
+					+ customer.getPassword() + "')";
+
 			int rows = stmt.executeUpdate(sql);
-			if(rows != 0) {
+			if (rows != 0) {
 				status = true;
 			}
 		} catch (SQLException sqle) {
@@ -83,7 +205,7 @@ public class CustomerDao extends Dao {
 			closeStatement(stmt);
 			closeConnection(con);
 		}
-		
+
 		return status;
 	}
 }
